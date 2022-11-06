93ab5243b75d4f4ed48ad57353ed30e3be27591c
Andrii Dubovyk <and.dubovyk@gmail.com>
2022-11-06 10:06:39 +0200

    Add gold as currency

--- Game/Assets/Scripts/Player.cs
+++ Game/Assets/Scripts/Player.cs
@@ -1,13 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]
+    private int startGold;
+    [SerializeField]
+    private TextMeshProUGUI goldUIText;
+
+    private int currentGold;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentGold = startGold;
+        goldUIText.text = $"Gold: {currentGold}";
+    }
+
+    void AddGold(int gold)
+    {
+        currentGold += gold;
+    }
+
+    void RemoveGold(int gold)
+    {
+        currentGold -= gold;
+    }
+
+    int GetGold()
+    {
+        return currentGold;
     }
 
     // Update is called once per frame
