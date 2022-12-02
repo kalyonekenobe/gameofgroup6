89c6dbea09d705d29f6155d970604ca5350c6551
Oleksandr Igumnov <igumssukr@gmail.com>
2022-12-02 14:50:09 +0200

    Fixed maps layers and code bugs

--- Game/Assets/Scripts/Enemy.cs
+++ Game/Assets/Scripts/Enemy.cs
@@ -86,21 +86,22 @@ public class Enemy : MonoBehaviour
         Vector2 point = routePoints[nextPointIndex];
         float distanceX = point.x - transform.position.x;
         float distanceY = point.y - transform.position.y;
+        float absDistanceX = Mathf.Abs(distanceX);
+        float absDistanceY = Mathf.Abs(distanceY);
 
         if (nextPointIndex >= routePoints.Length - 1)
             Destroy(gameObject);
 
-        if (Mathf.Abs(distanceX) < enemyType.moveSpeed * Time.deltaTime && Mathf.Abs(distanceY) < enemyType.moveSpeed * Time.deltaTime)
+        if (absDistanceX < enemyType.moveSpeed * Time.deltaTime && absDistanceY < enemyType.moveSpeed * Time.deltaTime)
         {
             nextPointIndex++;
             return;
         }
 
-        float directionX = distanceX / Mathf.Abs(distanceX) * Mathf.Min(Mathf.Abs(distanceX) / Mathf.Abs(distanceY), 1);
-        float directionY = distanceY / Mathf.Abs(distanceY) * Mathf.Min(Mathf.Abs(distanceY) / Mathf.Abs(distanceX), 1);
+        float directionX = (absDistanceX < 1e-20 ? 1 : (distanceX / absDistanceX)) * Mathf.Min(absDistanceX / (absDistanceY < 1e-20 ? absDistanceX : absDistanceY), 1);
+        float directionY = (absDistanceY < 1e-20 ? 1 : (distanceY / absDistanceY)) * Mathf.Min(absDistanceY / (absDistanceX < 1e-20 ? absDistanceY : absDistanceX), 1);
 
         Vector2 movementDirection = new Vector2(directionX * enemyType.moveSpeed * Time.deltaTime, directionY * enemyType.moveSpeed * Time.deltaTime);
-
 
         if (!rotated && distanceX < 0){ transform.Rotate(0, 180, 0); rotated = true;} 
         else if (rotated && distanceX >= 0) {rotated = false; transform.Rotate(0, 180, 0); }
