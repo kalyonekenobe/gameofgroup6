63d84b41bcd8124c2d6a9b798b57d791b7cce71d
Liliia Parashchak <liliia.parashchak@gmail.com>
2022-12-03 14:30:19 +0200

    Add watermalon rain

---
+++ Game/Assets/Scripts/WatermelonController.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WatermelonController : MonoBehaviour
+{
+    [SerializeField]
+    private double cooldown = 20;
+    private double currentTime;
+
+    [SerializeField]
+    private Button button;
+    [SerializeField]
+    private GameObject waterMelon;
+
+    private bool placeRain = false;
+    //private bool isAvailable;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = cooldown;
+        button.enabled = true;
+        button.GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (placeRain)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                PlaceMelonor();
+                button.enabled = false;
+                placeRain = false;
+            }
+        }
+        else
+        {
+            if (currentTime <= 0)
+            {
+                button.enabled = true;
+                button.GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+            }
+            else
+            {
+                currentTime -= Time.deltaTime;
+            }
+        }
+    }
+
+    public void ActivateRain()
+    {
+        placeRain = true;
+        button.enabled = false;
+        button.GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, 0.5f);
+        currentTime = cooldown;
+    }
+
+    private void PlaceMelonor()
+    {
+        Vector3 objPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        objPos.z = 1;
+        Instantiate(waterMelon, objPos, Quaternion.identity);
+    }
+}
